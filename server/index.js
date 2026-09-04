ab6970962c2f43573493f626ee5b7405fac37c30
oruchankural <oruchankural@gmail.com>
2026-09-04 23:44:40 +0300

    feat: cors removed

--- server/index.js
+++ server/index.js
@@ -1,18 +1,11 @@
 ﻿const express = require('express');
 const http = require('http');
 const {Server} = require('socket.io');
-const cors = require('cors');
 
 const app = express();
-app.use(cors());
 
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST"]
-    }
-});
+const io = new Server(server);
 const rooms = {};
 const SIDES = ['one', 'two', 'three', 'four', 'five', 'six'];
 const generateDice = (count) => Array.from({length: count}, () => SIDES[Math.floor(Math.random() * SIDES.length)]);
