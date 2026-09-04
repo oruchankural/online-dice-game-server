e02f9ae69c3b87053671f25084eb622c6b16c91f
oruchankural <oruchankural@gmail.com>
2026-09-04 23:48:12 +0300

    feat: allow any for cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 ﻿const express = require('express');
 const http = require('http');
 const {Server} = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 const rooms = {};
 const SIDES = ['one', 'two', 'three', 'four', 'five', 'six'];
 const generateDice = (count) => Array.from({length: count}, () => SIDES[Math.floor(Math.random() * SIDES.length)]);
